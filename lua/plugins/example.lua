375d066726025120cc4c70d342b80a02f59916fb
ryuji-1to <ryuji0222ss@icloud.com>
2025-02-08 18:11:13 +0900

    update

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -348,6 +348,25 @@ return {
       end
     end,
   },
+  -- {
+  --   "mrcjkb/rustaceanvim",
+  --   opts = function(_, opts)
+  --     opts = opts or {}
+  --     local local_opts = opts
+  --     local path = { "server", "default_settings", "rust-analyzer", "procMacro", "ignored" }
+  --
+  --     for i = 1, #path - 1 do
+  --       local_opts[path[i]] = local_opts[path[i]] or {}
+  --       local_opts = local_opts[path[i]]
+  --     end
+  --     local_opts[path[#path]] = {
+  --       ["napi-derive"] = { "napi" },
+  --       ["async-recursion"] = { "async_recursion" },
+  --     }
+  --
+  --     return opts
+  --   end,
+  -- },
   -- Haskell
   {
     "mrcjkb/haskell-tools.nvim",
